f982264664f25d5ce18bc7cdbceb74a2c7954938
DevSecOps Workshop <rlk7fq0gks@ddtraining.datadoghq.com>
2024-06-25 19:20:30 +0000

    apply SAST fixes

--- database.py
+++ database.py
@@ -24,14 +24,11 @@ def add_product(db_connection, product: Product):
         cursor = db_connection.cursor()
         # This is a potential SQL injection, as the product name could be directly passed from the user.
         # replace it with the line below:
-        # cursor.execute("INSERT INTO products (id, title) VALUES (NULL, ?)", (product.name, ))
-        cursor.execute("INSERT INTO products (id, title) VALUES (NULL, '" + product.name+"');")
+        cursor.execute("INSERT INTO products (id, title) VALUES (NULL, ?)", (product.name, ))
         db_connection.commit()
         return True
     # A bare exception fails to captures the exact exceptions (and print it). The code should rather
     # catch specific exceptions.
     # https://docs.datadoghq.com/code_analysis/static_analysis_rules/python-best-practices/no-bare-except/
     # Replace with the following: 
-    # except sqlite3.IntegrityError:
-    except:
-        return False
+    except sqlite3.IntegrityError:
